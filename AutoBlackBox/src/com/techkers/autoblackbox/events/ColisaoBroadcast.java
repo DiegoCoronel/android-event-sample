35e7239b26aec1202a48fe053d81777cc0e1af9b
unknown <joel.aragao@Lap-JoelAragao.dom.fpf.br>
2015-04-29 23:54:17 -0400

    adicionado evento de colisão.

---
+++ AutoBlackBox/src/com/techkers/autoblackbox/events/ColisaoBroadcast.java
@@ -0,0 +1,14 @@
+package com.techkers.autoblackbox.events;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class ColisaoBroadcast extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		
+	}
+
+}
